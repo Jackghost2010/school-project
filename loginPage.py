36a1c9a6c682b285def2b84d8ed588cec8786548
Jackghost2010 <joseastman2010@gmail.com>
2026-05-18 06:58:45 -0400

    Update loginPage.py

--- loginPage.py
+++ loginPage.py
@@ -1,8 +1,28 @@
+from random import randint
+import os
+
 ###the first file
 
+
+#### classes
 class person:
 
-    def __init__(self,int = age,string = name):
+    def __init__(self,age,name):
         self.age = age
-
         self.name = name
+
+
+##### functions
+
+
+
+def logins():
+    while True:
+        fistQuestion = input("hello mate what's your name? ")
+        secondQuestion = int(input("well what's you age? "))
+        user = person(secondQuestion,fistQuestion)
+        print("name " + user.name + " age " + str(user.age))
+        break
+
+logins()
+
