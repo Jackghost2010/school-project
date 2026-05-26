20178edfe3b506450a9302cef5ec8ebeb10fa08f
Jackghost2010 <joseastman2010@gmail.com>
2026-05-26 05:10:35 -0400

    Update loginPage.py

--- loginPage.py
+++ loginPage.py
@@ -1,37 +1,81 @@
-import tkinter as tk
-from os import system
-from os import chdir
-from random import randint
-
-"""
-this is how to create a title
-label = tk.Label(root,text = "login screen")
-
-label.pack()
-
-buttom 
-
-root = tk.Tk()
-root.title("Counting Seconds")
-
-button = tk.Button(root, text="Stop", width=25, command=root.destroy)
-button.pack()
-
-root.mainloop()
-
-"""
-
-root = tk.Tk()
-
-label = tk.Label(root,text = "login screen", width= 140)
-
-
-button = tk.Button(root, text="Stop", width=25, command=root.destroy)
-label.pack()
-
-button.pack()
+try:    
+    import tkinter as tk
+    from random import randint
+    from os import path
+    from os import chdir
+    from os import system
+    import hashlib
 
 
 
-root.mainloop()
+    def submits(username):
 
+        with open("text.txt", "w") as file:
+            file.write()
+
+    
+    def register(root):
+
+        root.title("register")  # Set the window title
+        canvas = tk.Canvas(root,height = 500, width = 700).pack()
+        section = tk.Label(root, text="register").pack()
+
+
+        username_labels = tk.Label(root,text="username").pack()  # Create a label
+        username = tk.Entry(root).pack()
+        password_labels = tk.Label(root,text="password").pack()  # Create a label
+        password = tk.Entry(root,show="*").pack()
+
+        submit = tk.Button(root,text="submit information" ).pack()
+
+
+
+
+        exited = tk.Button(root,text="exit the code",command="destroy.root").pack()
+
+
+
+
+
+
+
+
+
+        
+
+
+    def login(root):
+        root.title("login")  # Set the window title
+        canvas = tk.Canvas(root,height = 500, width = 700)
+        label = tk.Label(root, text="login")  # Create a label
+        label.pack()  # Add the label to the window
+        canvas.pack()
+
+
+    def mainPage(root):
+
+        root = tk.Tk()
+        canvas = tk.Canvas(root).pack()
+        label = tk.Label(root, text="works").pack()
+
+        root.mainloop() 
+
+
+    def main():
+            
+        root = tk.Tk()  # Create the main window
+
+        with open("check.txt", "r") as f:
+            if f.read() == "":
+                register(root)
+            else:
+                login(root)
+
+
+        root.mainloop()  # Start the Tkinter event loop
+    main()
+        
+        
+        
+except ImportError:
+    print("Tkinter is not installed")
