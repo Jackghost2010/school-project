8b7ab51c53b99fb2c976ba3fa7a7aeda57f78831
Jackghost2010 <joseastman2010@gmail.com>
2026-06-05 03:11:11 -0400

    Refactor login.py by cleaning up code and adding functions
    Removed unused imports and functions, added loginProccess and main functions.

--- login.py
+++ login.py
@@ -1,18 +1,15 @@
 from random import randint
 import sqlite3
 from pathlib import Path
-import hashlib
-from os import system
-from os import chdir
-
-def register_page():
-    
-    user = input("name: ")
-
-
-    password = getpass("password: ")
-
-
-    hashing_func(password)
-
+import ctype
+def loginProccess():
     pass
+def main():
+    conn = sqlite3.connect("userdate.db")
+    cursor = conn.cursor()
+    cursor.execute("create table if not exists userinfo (username TEXT, password TEXT)")
+    conn.close()
+    
+    
+
+
