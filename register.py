7b7299d1ad5fed5c9356c275a06f2255b26cad6e
Jackghost2010 <joseastman2010@gmail.com>
2026-06-05 03:11:25 -0400

    Update register.py

--- register.py
+++ register.py
@@ -1,48 +1,45 @@
-from random import randint
-import sqlite3
-from pathlib import Path
-import hashlib
-from os import system
-from os import chdir
-from getpass import getpass
-from hashing import hashing_func
+try:
+    from random import randint
+    import sqlite3
+    from pathlib import Path
+    from os import system
+    from os import chdir
+    from getpass import getpass
+    from hashing import hashing_func
+    from classes import user
 
-class user:
-    def __init__(self,username,password,status = "user"):
-        self.username = username
-        self.password = password
-        self.status = status
-
-        self.checking = "The username: " + username + " The password is: " + password +" It is status " + status
-
-
-
-
-def register_page():
-    username = input("username: ")
-    password = getpass("password: ")
-    userinfor = user(username,hashing_func(password))
-
-
-
-
-
-
-
-
-def check(directory,key):
-    check_directory_exist = Path(directory)
-    key_file_exist = Path(key)
-
-    if check_directory_exist.is_dir():
-        if key_file_exist.is_file():
-            pass
+    def register_page():
+        conn = sqlite3.connect("userdata.db")
+        
+        username = input("username: ")
+        password = getpass("password: ")
+        userinfor = user(username,hashing_func(password))
+        print(userinfor.username, userinfor.password, userinfor.status)
+        
+        
+        
+    def check():
+        
+        directory = "key_directory"
+        key = "being_here_before"
+        
+        check_directory_exist = Path(directory)
+        check_key_exist = Path(f"\/{directory}\/{key}")
+        if check_directory_exist.is_dir():
+            if check_key_exist.is_file():
+                pass
+            else:
+                system("touch registeBefore")
+                register_page()
         else:
-            system("touch registeBefore")
+            system(f"mkdir {directory}")
+            chdir(f"{directory}")
+            system("ls -la")
+            system(f"touch registeBefore.txt")
             register_page()
-    else:
-        system(f"mkdir {directory}")
-        chdir(f"{directory}")
-    
-
-
+            
+    check()
+            
+except ImportError:
+    print("something wrong with the modules")
+            
